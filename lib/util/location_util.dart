c140cff661aaa3fef6fd5056be453820ee03d135
Maitê <81782090+maitecr@users.noreply.github.com>
2025-04-09 16:08:30 -0300

    Update location_util.dart

--- lib/util/location_util.dart
+++ lib/util/location_util.dart
@@ -5,7 +5,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
 //const GOOGLE_API_KEY = [YOUR_GOOGLE_API_HERE];
-const GOOGLE_API_KEY = 'AIzaSyDauLtUwQ2iTy7c937x3V0V3N68XE7AYjg';
 
 class LocationUtil {
   static String generateLocationPreviewImage({
